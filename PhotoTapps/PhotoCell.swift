de9d6de8f81726c286588078b3792c6c85b72837
Egor Aleksandrovich <egor09662@gmail.com>
2022-12-11 19:46:33 +0100

    second commit

---
+++ PhotoTapps/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.swift
+//  PhotoTapps
+//
+//  Created by egor aleksandrovich on 11.12.2022.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var dogImageView: UIImageView!
+}
